927dad46c22b795adc9524c86c85543ca3f785b3
Bima Rizqy Ramadhan <bimarizqyramadhan@gmail.com>
2025-09-06 01:53:35 +0700

    feat: validation on google ads

--- src/modules/google-ads/dto/get-campaign-by-id.dto.ts
+++ src/modules/google-ads/dto/get-campaign-by-id.dto.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString } from 'class-validator';
 
 export class GetCampaignByIdDto {
+  @IsDateString({ strict: true })
   @ApiProperty({
     example: '2023-01-01',
   })
   start_date: string;
 
+  @IsDateString({ strict: true })
   @ApiProperty({
     example: '2025-01-31',
   })
